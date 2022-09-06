b8bc9515a5f60f87b2909da517222a5f020d6c77
Ajdev11 <mosesogun11@gmail.com>
2022-09-06 12:44:22 -0700

    Usercontext for passing props from nested components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 //import ClickCounter from "./clickCounter";
-import MouseHover from "./Components/MouseHover";
-import ClickCount from "./Components/clickCount";
+//import MouseHover from "./Components/MouseHover";
+//import ClickCount from "./Components/clickCount";
 //import Hover from "./Components/Hover";
-import User from "./Components/User";
-import Counter from "./Components/Counter";
+//import User from "./Components/User";
+//import Counter from "./Components/Counter";
+import ComponentC from "./Components/ComponentC";
+import { UserProvider } from "./Components/userContext";
+
 
 class App extends Component {
   render() {
     return (
       /*<>
+      <ClickCounter name="Olaseni"/>
       <ClickCounter name="Olaseni"/>
       <MouseHover />
       <ClickCount />
@@ -18,10 +22,13 @@ class App extends Component {
       <ClickCount />
       <MouseHover />
       <User name= {(isLoggedIn)=>isLoggedIn? "Moses":"Guest"}/>
+       <Counter name={(count, clickIncrement)=> <ClickCount count ={count} clickIncrement={clickIncrement} {...this.props} />}/>
+      <Counter name={(count, clickIncrement)=> <MouseHover count ={count} clickIncrement={clickIncrement} />}/>
       </>*/
       <>
-      <Counter name={(count, clickIncrement)=> <ClickCount count ={count} clickIncrement={clickIncrement} {...this.props} />}/>
-      <Counter name={(count, clickIncrement)=> <MouseHover count ={count} clickIncrement={clickIncrement} />}/>
+      <UserProvider value="Baddest developer">
+      <ComponentC />
+      </UserProvider>
       </>
     );
   }
