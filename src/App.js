daf5d89e6c1e1324ea1978ceab8c31ffbcc58f29
Ajdev11 <mosesogun11@gmail.com>
2022-09-06 15:10:10 -0700

    using useState for counting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import HooksCounter from "./HooksComponents/HooksCounter";
 //import ClickCounter from "./clickCounter";
 //import MouseHover from "./Components/MouseHover";
 //import ClickCount from "./Components/clickCount";
 //import Hover from "./Components/Hover";
 //import User from "./Components/User";
 //import Counter from "./Components/Counter";
-import ComponentC from "./Components/ComponentC";
-import { UserProvider } from "./Components/userContext";
+//import ComponentC from "./Components/ComponentC";
+//import { UserProvider } from "./Components/userContext";
 
 
 class App extends Component {
@@ -24,11 +25,12 @@ class App extends Component {
       <User name= {(isLoggedIn)=>isLoggedIn? "Moses":"Guest"}/>
        <Counter name={(count, clickIncrement)=> <ClickCount count ={count} clickIncrement={clickIncrement} {...this.props} />}/>
       <Counter name={(count, clickIncrement)=> <MouseHover count ={count} clickIncrement={clickIncrement} />}/>
-      </>*/
-      <>
       <UserProvider value="Baddest developer">
       <ComponentC />
       </UserProvider>
+      </>*/
+      <>
+      <HooksCounter />
       </>
     );
   }
