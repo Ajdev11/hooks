fa32f1ebf8b9b46e561380525df9d60080d4f3ca
Ajdev11 <mosesogun11@gmail.com>
2022-09-09 13:08:14 -0700

    useEffect rendering

--- src/HooksComponents/HooksEffect1.jsx
+++ src/HooksComponents/HooksEffect1.jsx
@@ -2,11 +2,14 @@ import React, {useState, useEffect} from 'react'
 
 function HooksEffect1() {
     const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
     useEffect(()=>{
+      console.log('useeffect')
       document.title = `You clciked ${count} times`
-    })
+    }, [count])
   return (
     <div>
+      <input type="text" value={name} onChange={e=>setName(e.target.value)} />
       <button onClick={()=>setCount(count + 1)}>You Clicked {count} times</button>
     </div>
   )
